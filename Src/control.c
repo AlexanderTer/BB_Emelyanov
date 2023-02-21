36b0689c8d9bfd9a268d997635beb224ff52749a
AlexanderTer <46087309+AlexanderTer@users.noreply.github.com>
2023-02-21 14:12:40 +0300

    Контур напряжения настроен

--- Src/control.c
+++ Src/control.c
@@ -41,13 +41,13 @@ Control_Struct BB_Control =
 		.pid_voltage =
 		{
 				.kp_boost = 4.5902f,
-				.kp_buck = 0.0f,
+				.kp_buck = 0.2724f,
 				.kp = 4.5902f,
 
 				.integrator =
 				{
 						.k_boost = 1.2668e4f * T_CALC,
-						.k_buck = 10.f * T_CALC,
+						.k_buck =  2.9269e+04f * T_CALC,
 						.k =   1.2668e4f * T_CALC,
 						.sat = {.min = 0, .max = 14.5f},
 				},
@@ -59,7 +59,7 @@ Control_Struct BB_Control =
 					.k = 0.f * F_CALC,
 
 				},
-				.sat = {.min = 0, .max = 14.5f},
+				.sat = {.min = 0, .max = 5.5f},
 		},
 
 };
@@ -90,13 +90,13 @@ Measure_Struct BB_Measure =
 		.dac[0] =
 		{
 				.shift = 0.f,
-				.scale = 4095.f / 7.f,
+				.scale = 4095.f / 1.f,
 		},
 
 		.dac[1] =
 		{
 				.shift = 0.f,
-				.scale = 4095.f / 22.f,
+				.scale = 4095.f / 1.f,
 		},
 
 };// end Measure_Struct BB_Measure ------------------------------------------
@@ -128,11 +128,11 @@ void HRTIM1_TIME_IRQHandler(void){
 	BB_Measure.data.inj = BB_Measure.scale.inj * ADC2->JDR1 + BB_Measure.shift.inj;
 	BB_Measure.data.uout = BB_Measure.scale.uout * (ADC1->DR + BB_Measure.shift.uout);
 	BB_Control.error_voltage = BB_Control.uout_ref - BB_Measure.data.uout;
-	BB_Control.iL_ref = PID_Controller(&BB_Control.pid_voltage,BB_Control.error_voltage);
+	float iref_b = PID_Controller(&BB_Control.pid_voltage,BB_Control.error_voltage);
+	BB_Control.iL_ref = iref_b + BB_Measure.data.inj;
 
 
 	// ----- Расчёт контура тока ---------
-	BB_Control.iL_ref = 5.f + 	BB_Measure.data.inj;
 	BB_Measure.data.iL = BB_Measure.scale.iL * ADC2->DR;
 	BB_Control.error_current = BB_Control.iL_ref - BB_Measure.data.iL;
 	BB_Control.duty = PID_Controller(&BB_Control.pid_current,BB_Control.error_current);
@@ -140,7 +140,7 @@ void HRTIM1_TIME_IRQHandler(void){
 
 	// Вывод данных на ЦАП1 ЦАП2
 	DAC1->DHR12R2 =  BB_Control.iL_ref  * BB_Measure.dac[0].scale; // DAC1 CH2  X16
-	DAC2->DHR12R1 =  BB_Measure.data.uout *  BB_Measure.dac[1].scale; // DAC2 CH1  X17
+	DAC2->DHR12R1 =  iref_b *  BB_Measure.dac[1].scale; // DAC2 CH1  X17
 
 
 
